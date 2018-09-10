c6a3e52ea8741a68637acef51a8f27e357a43841
ilebedev-ru <wapstyle@bk.ru>
2018-09-10 21:03:16 +0500

    создаем товары и вставляем в разметку

--- js/goods.js
+++ js/goods.js
@@ -68,7 +68,7 @@ var AMOUNTS = {
 };
 
 var PRICES = {
-  min:  100,
+  min: 100,
   max: 1500
 };
 
@@ -85,7 +85,7 @@ var RATING_VALUES = {
 var RATING_NUMBERS = {
   min: 10,
   max: 900
-}
+};
 
 var NUTRITION_FACTS_ENERGYS = {
   min: 70,
@@ -113,39 +113,43 @@ var NUTRITION_FACTS_CONTENT = [
   'виллабаджо',
 ];
 
+var numberOfGoods = NAMES.length;
+var NUMBER_OF_GOODS_IN_CARD = 3;
+
 var ratings = [RATING_VALUES, RATING_NUMBERS];
 var nutritionFacts = [NUTRITION_FACTS_ENERGYS, NUTRITION_FACTS_CONTENT];
 
 var goods = [NAMES, PICTURES, AMOUNTS, PRICES, WEIGHTS, ratings, nutritionFacts];
 
 
-var getRamdomArrIndex = function(arr) {
-  return Math.floor(Math.random() * arr.length);
+var getRamdomArrElement = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
 var getRandomNumber = function (min, max) {
-  var rand = min - 0.5 + Math.random() * (max - min + 1)
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
   rand = Math.round(rand);
   return rand;
 };
 
-var getRandomBoolean = function() {
+var getRandomBoolean = function () {
   return !!Math.round(Math.random());
 };
 
-var getRandomString = function(arr) {
+var getRandomString = function (arr) {
   var randomString = '';
   var stringQuantity = getRandomNumber(1, arr.length);
-  for(var i = 0; i < stringQuantity; i++) {
+  for (var i = 0; i < stringQuantity; i++) {
     randomString += (arr[i] + ', ');
   }
-  return randomString = randomString.substring(0, randomString.length - 2);
+  randomString.substring(0, randomString.length - 2);
+  return randomString;
 };
 
-var createGoodsItem = function(arr) {
+var createGoodsItem = function (arr) {
   var GoodsItem = {
-    name: arr[0][getRamdomArrIndex(arr[0])],
-    picture: arr[1][getRamdomArrIndex(arr[1])],
+    name: getRamdomArrElement(arr[0]),
+    picture: getRamdomArrElement(arr[1]),
     amount: getRandomNumber(arr[2].min, arr[2].max),
     price: getRandomNumber(arr[3].min, arr[3].max),
     weight: getRandomNumber(arr[4].min, arr[4].max),
@@ -162,42 +166,104 @@ var createGoodsItem = function(arr) {
   return GoodsItem;
 };
 
-
-var goodsItems = [];
-for (var i = 0; i < NAMES.length; i++) {
-  goodsItems[i] = createGoodsItem(goods);
+var createGoodsCollection = function (arr, num) {
+  var collection = [];
+  for (var i = 0; i < num; i++) {
+    collection[i] = createGoodsItem(arr);
+  }
+  return collection;
 };
 
-var catalogCards = document.querySelector('.catalog__cards');
+var goodsItems = createGoodsCollection(goods, numberOfGoods);
 
+var catalogCards = document.querySelector('.catalog__cards');
 catalogCards.classList.remove('catalog__cards--load');
 catalogCards.querySelector('.catalog__load').classList.add('visually-hidden');
 
 var catalogCardTemplate = document.querySelector('#card').content.querySelector('.catalog__card');
-
 
 var renderGoodsElement = function (item) {
   var goodsElement = catalogCardTemplate.cloneNode(true);
 
   goodsElement.querySelector('.card__title').textContent = item.name;
   goodsElement.querySelector('.card__img').src = item.picture;
-  goodsElement.querySelector('.card__price').innerHTML = item.price + ' <span class="card__currency">₽</span><span class="card__weight">' + item.weight + ' Г</span>';
-  // console.log(goodsElement.querySelector('.card__price'));
-  // goodsElement.querySelector('.card__weight').textContent = item.weight;
-  // goodsElement.querySelector('.star__count').textContent = item.Rating.number;
-  // goodsElement.querySelector('.card__composition-list').textContent = item.NutritionFact.contents;
+  goodsElement.querySelector('.card__img').alt = item.name;
+  goodsElement.querySelector('.card__price').innerHTML = item.price
+    + ' <span class="card__currency">₽</span><span class="card__weight">'
+    + item.weight
+    + ' Г</span>';
+
+  goodsElement.classList.remove('card--in-stock');
+  if (item.amount > 5) {
+    goodsElement.classList.add('card--in-stock');
+  } else if (item.amount <= 5 && item.amount >= 1) {
+    goodsElement.classList.add('card--little');
+  } else if (item.amount === 0) {
+    goodsElement.classList.add('card--soon');
+  }
+
+  goodsElement.querySelector('.stars__rating').classList.remove('stars__rating--five');
+  if (item.Rating.value === 1) {
+    goodsElement.querySelector('.stars__rating').classList.add('stars__rating--one');
+  } else if (item.Rating.value === 2) {
+    goodsElement.querySelector('.stars__rating').classList.add('stars__rating--two');
+  } else if (item.Rating.value === 3) {
+    goodsElement.querySelector('.stars__rating').classList.add('stars__rating--three');
+  } else if (item.Rating.value === 4) {
+    goodsElement.querySelector('.stars__rating').classList.add('stars__rating--four');
+  } else if (item.Rating.value === 5) {
+    goodsElement.querySelector('.stars__rating').classList.add('stars__rating--five');
+  }
+
+  goodsElement.querySelector('.star__count').textContent = item.Rating.number;
+
+  if (item.NutritionFact.sugar) {
+    goodsElement.querySelector('.card__characteristic').textContent = 'Содержит сахар';
+  } else {
+    goodsElement.querySelector('.card__characteristic').textContent = 'Без сахара';
+  }
+
+  goodsElement.querySelector('.card__composition-list').textContent = item.NutritionFact.contents;
 
   return goodsElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < goodsItems.length; i++) {
-  fragment.appendChild(renderGoodsElement(goodsItems[i]));
-}
+var createGoodsFragment = function (items) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < items.length; i++) {
+    fragment.appendChild(renderGoodsElement(items[i]));
+  }
+  return fragment;
+};
 
-catalogCards.appendChild(fragment);
+catalogCards.appendChild(createGoodsFragment(goodsItems));
 
+// CARD
+var goodsItemsInCard = createGoodsCollection(goods, NUMBER_OF_GOODS_IN_CARD);
 
-// + функцию генерации случайных данных,
-//функцию создания DOM-элемента на основе JS-объекта,
-//функцию заполнения блока DOM-элементами на основе массива JS-объектов.
+var goodsCards = document.querySelector('.goods__cards');
+goodsCards.classList.remove('goods__cards--empty');
+goodsCards.querySelector('.goods__card-empty').classList.add('visually-hidden');
+
+var goodsCardTemplate = document.querySelector('#card-order').content.querySelector('.goods_card');
+
+var renderGoodsInCardElement = function (item) {
+  var goodsInCardElement = goodsCardTemplate.cloneNode(true);
+
+  goodsInCardElement.querySelector('.card-order__title').textContent = item.name;
+  goodsInCardElement.querySelector('.card-order__img').src = item.picture;
+  goodsInCardElement.querySelector('.card-order__img').alt = item.name;
+  goodsInCardElement.querySelector('.card-order__price').textContent = item.price + ' ₽';
+
+  return goodsInCardElement;
+};
+
+var createGoodsInCardFragment = function (items) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < items.length; i++) {
+    fragment.appendChild(renderGoodsInCardElement(items[i]));
+  }
+  return fragment;
+};
+
+goodsCards.appendChild(createGoodsInCardFragment(goodsItemsInCard));
