5c7c2185371bdebc4520c41e591289369e2f864a
ilebedev-ru <wapstyle@bk.ru>
2018-10-01 20:26:13 +0500

    работа с фильтром по цене

--- js/goods.js
+++ js/goods.js
@@ -118,6 +118,9 @@ var GoodsData = {
 };
 
 var CVC_MIN = 100;
+var CARD_LENGTH = 16;
+var DATE_LENGTH = 5;
+var PIN_WIDTH = 10;
 
 var ratingStars = {
   1: 'stars__rating--one',
@@ -508,25 +511,79 @@ deliver.addEventListener('click', function (evt) {
   }
 });
 
-// первая фаза работы фильтра по цене
+// фильтр по цене
 (function () {
   var rangeFilter = document.querySelector('.range__filter');
   var rangeFilterWidth = getComputedStyle(rangeFilter).width.slice(0, -2);
 
+  var rangeLine = document.querySelector('.range__fill-line');
   var rangeBtnLeft = document.querySelector('.range__btn--left');
   var rangeBtnRight = document.querySelector('.range__btn--right');
   var rangePriceMin = document.querySelector('.range__price--min');
   var rangePriceMax = document.querySelector('.range__price--max');
 
-  rangeBtnLeft.addEventListener('mouseup', function () {
-    var rangeBtnLeftX = getComputedStyle(rangeBtnLeft).left.slice(0, -2);
-    rangePriceMin.textContent = rangeBtnLeftX / rangeFilterWidth * 100;
-  });
+  var mouseDownHandler = function (evt) {
+    evt.preventDefault();
+    var startCoordsX = evt.clientX;
+    var pinOptions = {};
+    if (evt.target.offsetLeft === rangeBtnLeft.offsetLeft) {
+      pinOptions = {
+        minX: 0,
+        maxX: rangeBtnRight.offsetLeft - PIN_WIDTH,
+        side: 'left'
+      };
+    } else if (evt.target.offsetLeft === rangeBtnRight.offsetLeft) {
+      pinOptions = {
+        minX: rangeBtnLeft.offsetLeft + PIN_WIDTH,
+        maxX: rangeFilterWidth,
+        side: 'right'
+      };
+    }
 
-  rangeBtnRight.addEventListener('mouseup', function () {
-    var rangeBtnRightX = getComputedStyle(rangeBtnRight).right.slice(0, -2);
-    rangePriceMax.textContent = 100 - rangeBtnRightX / rangeFilterWidth * 100;
-  });
+    var mouseMoveHandler = function (moveEvt) {
+      var shiftX = startCoordsX - moveEvt.clientX;
+      startCoordsX = moveEvt.clientX;
+
+      var newX = (evt.target.offsetLeft - shiftX);
+      if (newX < pinOptions.minX) {
+        newX = pinOptions.minX;
+      } else if (newX > pinOptions.maxX) {
+        newX = pinOptions.maxX;
+      }
+      var newValue = newX / rangeFilterWidth * 100;
+      evt.target.style.left = newValue + '%';
+
+      if (pinOptions.side === 'left') {
+        rangeLine.style.left = newValue + '%';
+        rangePriceMin.textContent = Math.round(GoodsData.PRICES.max * newValue / 100);
+      }
+
+      if (pinOptions.side === 'right') {
+        rangeLine.style.right = (100 - newValue) + '%';
+        rangePriceMax.textContent = Math.round(GoodsData.PRICES.max * newValue / 100);
+      }
+
+    };
+
+    var mouseUpHandler = function () {
+      if (pinOptions.side === 'left') {
+        rangePriceMin.textContent = Math.round(GoodsData.PRICES.max * evt.target.offsetLeft / rangeFilterWidth);
+      }
+
+      if (pinOptions.side === 'right') {
+        rangePriceMax.textContent = Math.round(GoodsData.PRICES.max * evt.target.offsetLeft / rangeFilterWidth);
+      }
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mousemove', mouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  };
+
+  rangeBtnLeft.addEventListener('mousedown', mouseDownHandler);
+  rangeBtnRight.addEventListener('mousedown', mouseDownHandler);
 })();
 
 // проверка банковской карты
@@ -563,7 +620,7 @@ deliver.addEventListener('click', function (evt) {
   });
 
   cardNumberInput.addEventListener('blur', function () {
-    if (cardNumberInput.value.length !== 16) {
+    if (cardNumberInput.value.length !== CARD_LENGTH) {
       cardNumberInput.style.borderColor = 'red';
       cardNumberInput.setCustomValidity('Номер карты должен содержать 16 цифр');
     } else {
@@ -584,7 +641,7 @@ deliver.addEventListener('click', function (evt) {
     if (dateInput.validity.patternMismatch) {
       dateInput.setCustomValidity('Введите дату в формате ММ/ГГ');
       dateInput.style.borderColor = 'red';
-    } else if (dateInput.value.length === 5) {
+    } else if (dateInput.value.length === DATE_LENGTH) {
       dateInput.setCustomValidity('');
       dateInput.style.borderColor = 'green';
     }
